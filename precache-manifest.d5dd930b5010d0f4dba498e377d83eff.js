838880995a21a925c211eef50a3860d9a0f82d61
CheenaT <chingiz.tursunaliyev@gmail.com>
2019-04-09 09:57:35 +0300

    Updates

---
+++ precache-manifest.d5dd930b5010d0f4dba498e377d83eff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf5e86a4c4192f86e9b0",
+    "url": "/super-heroes-gazprombank/static/css/main.6cf93eff.chunk.css"
+  },
+  {
+    "revision": "cf5e86a4c4192f86e9b0",
+    "url": "/super-heroes-gazprombank/static/js/main.757289ee.chunk.js"
+  },
+  {
+    "revision": "4e8ec285bff274f120b4",
+    "url": "/super-heroes-gazprombank/static/js/runtime~main.4da69c46.js"
+  },
+  {
+    "revision": "93d4c1d93ad8774f5b01",
+    "url": "/super-heroes-gazprombank/static/js/2.cc0c4ae5.chunk.js"
+  },
+  {
+    "revision": "0c80cdb8a5ba26c6ac2d61d9e64d0ef3",
+    "url": "/super-heroes-gazprombank/static/media/Marvel.0c80cdb8.svg"
+  },
+  {
+    "revision": "0ca8d69fdb966a500e51387b3c9155f5",
+    "url": "/super-heroes-gazprombank/static/media/HeroCounter.0ca8d69f.svg"
+  },
+  {
+    "revision": "f23e39e296ee19620c70418b2f3f730e",
+    "url": "/super-heroes-gazprombank/static/media/Ellipse-delete.f23e39e2.svg"
+  },
+  {
+    "revision": "d1294e486ed4c83314e63e2af3f0f822",
+    "url": "/super-heroes-gazprombank/static/media/Vector.d1294e48.svg"
+  },
+  {
+    "revision": "b06677034fd55e1982ac318949a97a96",
+    "url": "/super-heroes-gazprombank/static/media/dc-logo.b0667703.svg"
+  },
+  {
+    "revision": "7701067365988b773e463f8c9f8d272c",
+    "url": "/super-heroes-gazprombank/static/media/Ellipse.77010673.svg"
+  },
+  {
+    "revision": "5591022baa96c1bbf6d387c3703baebb",
+    "url": "/super-heroes-gazprombank/static/media/Rectangle.5591022b.svg"
+  },
+  {
+    "revision": "ab50d1a3adba163ac27324ff8e677dc4",
+    "url": "/super-heroes-gazprombank/index.html"
+  }
+];
